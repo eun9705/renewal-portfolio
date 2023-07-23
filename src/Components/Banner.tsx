e0888de95e77c2246e6cf270b4892b605dc598c0
eunkyoung <eun97052425@gmail.com>
2023-07-23 23:51:30 +0900

    modify:banner 이미지 위치 수정

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -12,22 +12,29 @@ const Banner = () => {
                 <p>기술스택, 경력 정보를 확인할 수 있어요.</p>
                 <Buttons name={"소개글 보기"} url={"/about"}/>
                 <Buttons name={"경력사항 보기"} url={"/work"}/>
-                {/* <img src={BannerIcon} alt="배너 이미지" /> */}
+                <img src={BannerIcon} alt="배너 이미지" />
             </div>
         </BannerWrapper>
     );
 };
 
 const BannerWrapper = styled(FlexCol)`
-    width:100vw;height:30rem;background-color:#D5E6FB;padding-left:10rem;
-    div { position:relative;width:164rem;}
+    width:100vw;height:30rem;background-color:#D5E6FB;
+    div { position:relative;width:85.4vw;max-width:164rem; }
     img { position:absolute;bottom:-71.5px;right:0; }
     strong { font-size:2.6rem; }
     p { margin:1rem 0 4rem;color:var(--gray02); }
     ${DeviceSize.large`
-        overflow:hidden;padding:0;
-        div { width:var(--container-size-basic); }
-        img { width:50vw; }
+        img { width:50%; }
+    `}
+    ${DeviceSize.medium`
+        img { bottom:-54px; }
+    `}
+    ${DeviceSize.small`
+        img { bottom:-16px; }
+    `}
+    ${DeviceSize.xsmall`
+        img { bottom:-22.5px; }
     `}
 `
 
