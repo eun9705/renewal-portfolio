5b1df2d50cee607df3a0ad4fbf333c01be253d65
eunkyoung <eun97052425@gmail.com>
2023-07-27 19:55:19 +0900

    add :  error 컴포넌트 추가

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,9 +1,30 @@
+import { styled } from "styled-components";
+import { FlexCol } from "../Style/GlobalStyle";
+import errorImg from '../Image/errorImg.svg';
+import { DeviceSize } from "../Style/Responsive";
+
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+        <ErrorWrapper>
+            <img src={errorImg} alt="에러이미지" />
+            <strong>Page Not Found</strong>
+            <p>입력한 주소가 잘못되었거나, <br className="mobile-block"/>사용이 일시 중단되어 요청하신 페이지를 찾을 수 없습니다.</p>
+        </ErrorWrapper>
     );
 };
+
+const ErrorWrapper =styled(FlexCol)`
+    height:100vh;
+    img { height:60%; }
+    strong {  font-size:4rem;color:var(--main); }
+    p { margin-top:1rem; font-weight:500;color:var(--gray01); }
+    ${DeviceSize.small`
+        img { height:auto; }
+    `}
+    ${DeviceSize.xsmall`
+        text-align:center;
+        img { width:80%; }
+    `}
+`
 
 export default NotFound;
