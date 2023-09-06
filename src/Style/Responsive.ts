253637511c943ecbe232bbcc67a19ed9e71d255e
eunkyoung <eun97052425@gmail.com>
2023-09-06 15:00:09 +0900

    delete : 주석 삭제x

--- src/Style/Responsive.ts
+++ src/Style/Responsive.ts
@@ -1,25 +1,3 @@
-// import { css,CSSProp } from 'styled-components';
-
-// interface BreakPoints {
-//     [key:string]:number;
-// }
-
-// const breakPointsProps:BreakPoints = {
-//     large:1441,
-//     medium:1181,
-//     small:821,
-//     xsmall:431
-// }
-
-// export const DeviceSize = Object.keys(breakPointsProps).reduce((acc, label) => {
-//     acc[label] = (literals: TemplateStringsArray, ...args:any[]) => css`
-//         @media only screen and (max-width: ${breakPointsProps[label]}px) {
-//             ${css(literals, ...args)};
-//         }
-//     `
-//     return acc
-// }, {} as Record<keyof typeof breakPointsProps, (literals: TemplateStringsArray, ...args: any[]) => CSSProp>);
-
 const customMediaQuery = (maxWidth: number) =>
   `@media only screen and (max-width: ${maxWidth}px)`;
 
